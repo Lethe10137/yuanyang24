a10be8b00e347ff189d8b2435e3e84fd58ada604
ChenghaoLi <lichenghaongyd@qq.com>
2024-01-29 18:40:41 +0000

    fix bug

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -48,7 +48,7 @@ def reply(request : HttpRequest, _):
             result = "查询id" + openid
         elif len(content) == 128:
             try:
-                id, time, question = decode_token(request["FromUserName"])
+                id, time, question = decode_token(request["FromUserName"], KEY_PHASE, SALT_PHASE)
                 if(id == openid):
                     result = "{} {} {}".format(id, time, question)
                 else:
