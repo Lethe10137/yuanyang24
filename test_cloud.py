5a420ef594f4031eb615c2c88e7b413f466207df
ChenghaoLi <lichenghaongyd@qq.com>
2024-02-02 13:33:08 +0000

    修正积分数值错误

--- test_cloud.py
+++ test_cloud.py
@@ -10,6 +10,13 @@ message = {
   "token" : "45197524"
 }
 
+message = {
+  "action": "submit", 
+  "openid": "a159de679ef026757e39bb42886bf6e8f46b3b3db7",
+  "token": "af58d2b608c5fc700bab6309140e2eef9b5eb7063d4c8cd583b5044d58f6acdf4db3d16a137b87489a0f8401c5028b7ccc03f2a7e9a59a9079dd2d9407117549"
+  # "token" : "45197524"
+}
+
 result = requests.post(
     # "http://127.0.0.1:8000/cloud/",
     # "https://django-bgq6-91011-7-1324104223.sh.run.tcloudbase.com/cloud/",
